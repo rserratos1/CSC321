09e4fb16d8d5cbe14aa92046ea1508c36a3e992c
rs <ros11@live.com>
2024-10-23 01:24:35 +0000

    lab7 part1

---
+++ rserratos7.py
@@ -0,0 +1,22 @@
+#Raquel Serratos
+
+x = 100
+y = 5.2
+s = "Done"
+
+if x > 100:
+    print ("The variable is larger than 100")
+elif x == 100:
+    print ("The variable is 100!")
+else:
+    print ("The variable is less than 100")
+
+if y > 5.0:
+    print ("The float is larger than 5.0")
+else:
+    print("The float is less than 5.0")
+
+if s == "Done":
+    print ("The password is Done")
+else:
+    print ("You did not guess the password")
